fec087821f4f4cc4405f6fa80318ccfa07054313
karimovemir-code <karimovemir88@gmail.com>
2025-09-13 12:26:29 +0500

    1213

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>if you say</h1>
-    <data value=""></data>
-    <p>11</p>
-    <h1>lorem ipsum oto set immaterium ave imperor</h1>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>if you say</h1>
+        <data value=""></data>
+        <p>11</p>
+        <h1>lorem ipsum oto set immaterium ave imperor</h1>
+        1
+    </body>
 </html> 
